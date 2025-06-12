525fdbbbd17b78355d65a938c78b69b8fcb86082
stqcky <xxillik@gmail.com>
2025-06-12 20:50:23 +0300

    fix: default nvim-tree mappings not working

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -12,6 +12,8 @@ return {
                         on_attach = function(bufnr)
                                 local api = require("nvim-tree.api")
 
+                                api.config.mappings.default_on_attach(bufnr)
+
                                 local function opts(desc)
                                  return { desc = "NvimTree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
                                 end
